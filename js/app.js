ff19a639cb16036691681b73e155ec2737510d64
Monica Salama <monicasalama6@gmail.com>
2018-02-23 15:50:20 +0200

    Seperated model from ModelView.

--- js/app.js
+++ js/app.js
@@ -1,27 +1,29 @@
 
+var cat = function() {
+  this.clickCount = ko.observable(0);
+  this.name = ko.observable('Tabby');
+  this.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
+
+  this.level = ko.computed(function() {
+      if (this.clickCount() <= 10) {
+        return "Baby";
+      }
+      if (this.clickCount() <= 20) {
+        return "Infant";
+      }
+      return "Teen";
+  }, this);
+
+  this.nickNames = ko.observableArray([
+    'Tabtab', 'Temon', 'Bomba'
+  ]);
+}
 
 var ViewModel = function (){
-    this.clickCount = ko.observable(0);
-    this.name = ko.observable('Tabby');
-    this.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
-
+    this.currentCat = ko.observable(new cat());
     this.incrementCount = function() {
-        this.clickCount(this.clickCount() + 1);
+        this.currentCat().clickCount(this.currentCat().clickCount() + 1);
     };
-
-    this.level = ko.computed(function() {
-        if (this.clickCount() <= 10) {
-          return "Baby";
-        }
-        if (this.clickCount() <= 20) {
-          return "Infant";
-        }
-        return "Teen";
-    }, this);
-
-    this.nickNames = ko.observableArray([
-      'Tabtab', 'Temon', 'Bomba'
-    ]);
 }
 
 ko.applyBindings(new ViewModel());
