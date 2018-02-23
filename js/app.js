67cc31495c06f4c29853ccf6909006a68d2cc205
Monica Salama <monicasalama6@gmail.com>
2018-02-23 23:05:44 +0200

    Added list of cats.

--- js/app.js
+++ js/app.js
@@ -1,8 +1,48 @@
 
-var cat = function() {
-  this.clickCount = ko.observable(0);
-  this.name = ko.observable('Tabby');
-  this.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
+var initialCats =  [
+        {
+            clickCount : 0,
+            name : 'Tabby',
+            imgSrc : 'img/434164568_fea0ad4013_z.jpg',
+            imgAttribution : 'https://www.flickr.com/photos/bigtallguy/434164568',
+            nickNames: ['Tabtab', 'Temon', 'Bomba']
+        },
+        {
+            clickCount : 0,
+            name : 'Tiger',
+            imgSrc : 'img/4154543904_6e2428c421_z.jpg',
+            imgAttribution : 'https://www.flickr.com/photos/xshamx/4154543904',
+            nickNames: ['Tabtab', 'Temon', 'Bomba']
+        },
+        {
+            clickCount : 0,
+            name : 'Scaredy',
+            imgSrc : 'img/22252709_010df3379e_z.jpg',
+            imgAttribution : 'https://www.flickr.com/photos/kpjas/22252709',
+            nickNames: ['Tabtab', 'Temon', 'Bomba']
+        },
+        {
+            clickCount : 0,
+            name : 'Shadow',
+            imgSrc : 'img/1413379559_412a540d29_z.jpg',
+            imgAttribution : 'https://www.flickr.com/photos/malfet/1413379559',
+            nickNames: ['Tabtab', 'Temon', 'Bomba']
+        },
+        {
+            clickCount : 0,
+            name : 'Sleepy',
+            imgSrc : 'img/9648464288_2516b35537_z.jpg',
+            imgAttribution : 'https://www.flickr.com/photos/onesharp/9648464288',
+            nickNames: ['Tabtab', 'Temon', 'Bomba']
+        }
+];
+
+var cat = function(data) {
+  this.clickCount = ko.observable(data.clickCount);
+  this.name = ko.observable(data.name);
+  this.imgSrc = ko.observable(data.imgSrc);
+  this.imgAttribution = ko.observable(data.imgAttribution);
+  this.nickNames = ko.observableArray(data.nickNames);
 
   this.level = ko.computed(function() {
       if (this.clickCount() <= 10) {
@@ -13,16 +53,23 @@ var cat = function() {
       }
       return "Teen";
   }, this);
-
-  this.nickNames = ko.observableArray([
-    'Tabtab', 'Temon', 'Bomba'
-  ]);
 }
 
-var ViewModel = function (){
-    this.currentCat = ko.observable(new cat());
+var ViewModel = function () {
+    var self = this;
+    this.catList = ko.observableArray([])
+    initialCats.forEach(function(catItem) {
+        self.catList.push(new cat(catItem));
+    });
+
+    this.currentCat = ko.observable(this.catList()[0]);
+
     this.incrementCount = function() {
-        this.currentCat().clickCount(this.currentCat().clickCount() + 1);
+        self.currentCat().clickCount(self.currentCat().clickCount() + 1);
+    };
+
+    this.setCat = function(clickedCat) {
+      self.currentCat(clickedCat);
     };
 }
 
